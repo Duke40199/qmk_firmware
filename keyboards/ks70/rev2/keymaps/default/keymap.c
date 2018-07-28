ceffb7020f5f07aca5ca43273374005405afbdd3
marhalloweenvt <marhalloweenvt@gmail.com>
2018-07-28 17:23:12 +0700

    Update keymap ks70

--- keyboards/ks70/rev2/keymaps/default/keymap.c
+++ keyboards/ks70/rev2/keymaps/default/keymap.c
@@ -95,11 +95,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_COLEMAK] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_5,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
-    ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
+    KC_DEL,  GRAVE_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
+    KC_PSCR,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
+    KC_PGUP, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
+    KC_PGDOWN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
+    KC_NUMLOCK,  KC_LCTL,  KC_LALT, KC_LGUI, KC_SPC,    KC_SPC,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
     ),
 
   /* Dvorak
@@ -116,11 +116,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_DVORAK] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_5,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
-    ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
+    KC_DEL,  GRAVE_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
+    KC_PSCR,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
+    KC_PGUP, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
+    KC_PGDOWN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
+    KC_NUMLOCK,  KC_LCTL,  KC_LALT, KC_LGUI, KC_SPC,    KC_SPC,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
     ),
 
   /* Lower
@@ -137,11 +137,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_5,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
-    ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
+    KC_DEL,  GRAVE_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
+    KC_PSCR,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
+    KC_PGUP, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
+    KC_PGDOWN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
+    KC_NUMLOCK,  KC_LCTL,  KC_LALT, KC_LGUI, KC_SPC,    KC_SPC,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
     ),
 
   /* Raise
@@ -158,11 +158,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_5,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
-    ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
+    KC_DEL,  GRAVE_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
+    KC_PSCR,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
+    KC_PGUP, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
+    KC_PGDOWN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
+    KC_NUMLOCK,  KC_LCTL,  KC_LALT, KC_LGUI, KC_SPC,    KC_SPC,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
     ),
 
   /* Adjust (Lower + Raise)
@@ -179,11 +179,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_ADJUST] =  LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_5,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
-    ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
+    KC_DEL,  GRAVE_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,    KC_DEL,    KC_DEL, \
+    KC_PSCR,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_5,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,    KC_DEL,    KC_DEL, \
+    KC_PGUP, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_5,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_DEL,    KC_DEL, \
+    KC_PGDOWN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RBRC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    KC_DEL,    KC_DEL, \
+    KC_NUMLOCK,  KC_LCTL,  KC_LALT, KC_LGUI, KC_SPC,    KC_SPC,   KC_SPC,  KC_SPC,      RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,    KC_DEL \
     )
 };
 
